176fc801297ddfc137e932ffc7711701673cbd80
Vinícius Lourenço <vinicius_lourenco@outlook.com>
2026-04-19 15:19:12 -0300

    test: add login fail test

--- cypress/e2e/auth/login.cy.js
+++ cypress/e2e/auth/login.cy.js
@@ -18,4 +18,19 @@ describe('Auth - Login', () => {
             expect(response.statusText).to.equal('OK');
         })
     })
+
+    it('should fail login', () => {
+        cy.request({
+            method: 'POST',
+            url: `${Cypress.config('baseUrl')}/login`,
+            body: {
+                email: user.email,
+                password: 'teste1'
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.equal(401);
+            expect(response.body).to.have.property('message').that.includes('inválidos')
+        })
+    })
 })
