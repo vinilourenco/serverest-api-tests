49f781a2a2d2f4ab493f07bb4a071ee190e96f43
Vinícius Lourenço <vinicius_lourenco@outlook.com>
2026-04-20 13:11:27 -0300

    test: add fail test when email and password fields are blank

--- cypress/e2e/auth/login.cy.js
+++ cypress/e2e/auth/login.cy.js
@@ -34,7 +34,7 @@ describe('Auth - Login', () => {
         })
     })
 
-    it('Shoudl fail login - wrong user name', () => {
+    it('Should fail login - wrong user name', () => {
         cy.request({
             method: 'POST',
             url: `${Cypress.config('baseUrl')}/login`,
@@ -46,6 +46,24 @@ describe('Auth - Login', () => {
         }).then((response) => {
             expect(response.status).to.equal(400)
             expect(response.body).to.have.property('email').includes('email deve ser um email válido')
+        })
+    })
+
+    it('Should fail login - email and password fields are blank', () => {
+        cy.request({
+            method: 'POST',
+            url: `${Cypress.config('baseUrl')}/login`,
+            body: {
+                email: '',
+                password: ''
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.equal(400)
+            expect(response.body).to.include.all.keys('email', 'password')
+                .and.to.satisfy(body => {
+                    return body.email.includes('branco') && body.password.includes('branco')
+                })
         })
     })
 })
