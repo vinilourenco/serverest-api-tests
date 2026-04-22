2a50776a16a6105f30e63f85e7cc0a66f9c6f63c
Vinícius Lourenço <vinicius_lourenco@outlook.com>
2026-04-22 12:49:43 -0300

    fix: remove fixed value for verification from quantidade property and add usuarios property

--- cypress/e2e/users/listUsers.cy.js
+++ cypress/e2e/users/listUsers.cy.js
@@ -1,12 +1,12 @@
 describe('Users - List Users', () => {
 
-    it('Should list all users', () => {
+    it.only('Should list all users', () => {
         cy.request({
             method: 'GET',
             url: `${Cypress.config('baseUrl')}/usuarios`
         }).then((response) => {
             expect(response.status).to.equal(200)
-            expect(response.body).to.have.property('quantidade').to.equal(9)
+            expect(response.body).to.include.all.keys('quantidade', 'usuarios')
         })
     })
-})
+}) 
