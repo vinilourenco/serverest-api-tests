170a57036b00553b57d54fff0c68604ba4ab42a8
Vinícius Lourenço <vinicius_lourenco@outlook.com>
2026-04-26 21:41:34 -0300

    test: add empty ID verification test

--- cypress/e2e/users/getUser.cy.js
+++ cypress/e2e/users/getUser.cy.js
@@ -11,4 +11,15 @@ describe('Users - Get Users', () => {
             expect(response.body).to.have.property('_id').includes(`${defaultId}`)
         })
     })
+
+    it('Bad Request - Should return error if ID param is empty', () => {
+        cy.request({
+            method: 'GET',
+            url: `${Cypress.config('baseUrl')}/usuarios/:id`,
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.equal(400)
+            expect(response.body).to.have.property('id').includes('id deve ter exatamente 16 caracteres alfanuméricos')
+        })
+    })
 })
