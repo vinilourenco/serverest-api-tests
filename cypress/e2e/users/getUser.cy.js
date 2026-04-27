1ac804b27d8b9359c2560339d75d1dbae33c3116
Vinícius Lourenço <vinicius_lourenco@outlook.com>
2026-04-27 12:51:20 -0300

    test: add verification when ID param exceeds normal lenght and non-exisent ID

--- cypress/e2e/users/getUser.cy.js
+++ cypress/e2e/users/getUser.cy.js
@@ -1,6 +1,8 @@
 describe('Users - Get Users', () => {
 
     const defaultId = '0uxuPY0cbmQhpEz1';
+    const nonExistentUser = '0uxuPY0cbmQhpEz2';
+    const longText = Cypress._.repeat('abcdefghijklmnopqrstuvwxyz', 20)
 
     it('OK - Verify that the user is found with a valid ID.', () => {
         cy.request({
@@ -26,11 +28,33 @@ describe('Users - Get Users', () => {
     it('Bad Request - Should return error if ID param has minimum value', () => {
         cy.request({
             method: 'GET',
-            url: `${Cypress.config('baseUrl')}/usuarios/:id`,
+            url: `${Cypress.config('baseUrl')}/usuarios/a`,
             failOnStatusCode: false
         }).then((response) => {
             expect(response.status).to.equal(400)
             expect(response.body).to.have.property('id').includes('id deve ter exatamente 16 caracteres alfanuméricos')
+        })
+    })
+
+    it('Bad Request - Should return error for strings that exceeds the normal length', () => {
+        cy.request({
+            method: 'GET',
+            url: `${Cypress.config('baseUrl')}/usuarios/${longText}`,
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.equal(400)
+            expect(response.body).to.have.property('id').includes('id deve ter exatamente 16 caracteres alfanuméricos')
+        })
+    })
+
+    it('Bad Request - Should return error for a non-existent user', () => {
+        cy.request({
+            method:'GET',
+            url: `${Cypress.config('baseUrl')}/usuarios/${nonExistentUser}`,
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.equal(400)
+            expect(response.body).to.have.property('message').includes('Usuário não encontrado')
         })
     })
 })
