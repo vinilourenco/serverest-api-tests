6c6033e5fc5b2845e120c38ec50b4e37683e71a1
Vinícius Lourenço <vinicius_lourenco@outlook.com>
2026-05-08 13:12:36 -0300

    test: ensure that name field cannot be missing

--- cypress/e2e/users/editUser.cy.js
+++ cypress/e2e/users/editUser.cy.js
@@ -47,45 +47,45 @@ describe('Users - Edit User', () => {
 
     const succesCases =[
         {
-            description: 'update user keeping the same email',
+            description: 'Should update user keeping the same email',
             user: { id: `${defaultId}`, nome: 'Modified Name', email: 'fulano@qa.com', password: 'novasenha123', administrador: 'true' }
         },
         {
-            description: 'update administrador value succesfully',
+            description: 'Should update administrador value succesfully',
             user: { id: `${defaultId}`, nome: 'Fulano da Silva', email: 'fulano@qa.com', password: 'teste', administrador: 'false' }
         },
         {
-            description: 'return 200 when using special characters for user editing',
+            description: 'Should return 200 when using special characters for user editing',
             user: { id: `${defaultId}`, nome: "Fúlano d'Sílva Jüñíor", email: 'fulano.special@qa.com', password: 'teste123', administrador: 'true' }
         },
         {
-            description: 'return 200 when updating user with a long name',
+            description: 'Should return 200 when updating user with a long name',
             user: { id: `${defaultId}`, nome: longName , email: 'long.name@qa.com', password: 'teste', administrador: 'true' }
         },
         {
-            description: 'return 200 for email with complex domain',
+            description: 'Should return 200 for email with complex domain',
             user: { id: `${defaultId}`, nome: 'Email Test' , email: 'user+tag@subdomain.enterprise.com.br', password: 'teste', administrador: 'true' }
         },
         {
-            description: 'return 200 for password with special characters',
+            description: 'Should return 200 for password with special characters',
             user: { id: `${defaultId}`, nome: 'Password Test' , email: 'special.password@qa.com', password: 'S3nh@!C0mpl3x@#$%', administrador: 'false' }
         },
         {
-            description: 'return 200 if password too short (1 char)',
+            description: 'Should return 200 if password too short (1 char)',
             user: { id: `${defaultId}`, nome: 'Short Password' , email: 'short.password@qa.com', password: '1', administrador: 'true' }
         },
         {
-            description: 'return 200 if password too long',
+            description: 'Should return 200 if password too long',
             user: { id: `${defaultId}`, nome: 'Long Password' , email: 'long.password@qa.com', password: longPassword, administrador: 'false' }
         },
         {
-            description: 'return 200 if email is uppercase',
+            description: 'Should return 200 if email is uppercase',
             user: { id: `${defaultId}`, nome: 'Uppercase Email' , email: 'UPPERCASE.TEST@QA.COM', password: 'teste', administrador: 'true' }
         }
     ]
 
     succesCases.forEach(({description, user}) => {
-        it(`OK - Should ${description} (P)`, () => {
+        it(`OK - ${description}`, () => {
             cy.editUser(user.id, user.nome, user.email, user.password, user.administrador).then((response) => cy.editUserSuccess(response))
             cy.editUser(`${defaultId}`, 'Fulano da Silva', 'fulano@qa.com', 'teste', 'true').then((response) => cy.editUserSuccess(response))
         })
@@ -93,7 +93,7 @@ describe('Users - Edit User', () => {
 
     const errorCases = [
         {
-            description: 'blank space on fields',
+            description: 'Should return error when blank space on fields',
             method: 'PUT',
             url: `${Cypress.config('baseUrl')}/usuarios/${defaultId}`,
             body: { nome: '  Fulano da Silva  ', email: '  fulano@qa.com  ', password: '  teste  ', administrador: 'true' },
@@ -101,23 +101,23 @@ describe('Users - Edit User', () => {
             property: 'email'
         },
         {
-            description: 'updating user with existent email',
+            description: 'Should return error when updating user with existent email',
             method: 'PUT',
             url: `${Cypress.config('baseUrl')}/usuarios/${defaultId2}`,
-            body: { nome: 'Usuário Duplicado', email: 'fulano@qa.com', password: 'teste', administrador: 'true' },
+            body: { nome: 'Duplicated User', email: 'fulano@qa.com', password: 'teste', administrador: 'true' },
             expected: { status: 400, message: 'Este email já está sendo usado' },
             property: 'message'
         },
         {
-            description: 'name field is empty',
+            description: 'Should return error when name field is empty',
             method: 'PUT',
             url: `${Cypress.config('baseUrl')}/usuarios/${defaultId}`,
-            body: { nome: '', email: 'nome.vazio@qa.com', password: 'teste', administrador: 'true' },
+            body: { nome: '', email: 'empty.name@qa.com', password: 'teste', administrador: 'true' },
             expected: { status: 400, message: 'nome não pode ficar em branco' },
             property: 'nome'
         },
         {
-            description: 'email field is empty',
+            description: 'Should return error when email field is empty',
             method: 'PUT',
             url: `${Cypress.config('baseUrl')}/usuarios/${defaultId}`,
             body: { nome: 'Fulano da Silva', email: '', password: 'teste', administrador: 'true' },
@@ -125,25 +125,33 @@ describe('Users - Edit User', () => {
             property: 'email'
         },
         {
-            description: 'password field is empty',
+            description: 'Should return error when password field is empty',
             method: 'PUT',
             url: `${Cypress.config('baseUrl')}/usuarios/${defaultId}`,
-            body: { nome: 'Fulano da Silva', email: 'senha.vazia@qa.com.br', password: '', administrador: 'false' },
+            body: { nome: 'Fulano da Silva', email: 'empty.password@qa.com.br', password: '', administrador: 'false' },
             expected: { status: 400, message: 'password não pode ficar em branco' },
             property: 'password'
         },
         {
-            description: 'administrador field is empty',
+            description: 'Should return error when administrador field is empty',
             method: 'PUT',
             url: `${Cypress.config('baseUrl')}/usuarios/${defaultId}`,
-            body: { nome: 'Fulano da Silva', email: 'admin.vazio@qa.com.br', password: 'teste', administrador: '' },
+            body: { nome: 'Fulano da Silva', email: 'empty.admin@qa.com.br', password: 'teste', administrador: '' },
             expected: { status: 400, message: "administrador deve ser 'true' ou 'false'" },
             property: 'administrador'
+        },
+        {
+            description: 'Should return error when name field is missing',
+            method: 'PUT',
+            url: `${Cypress.config('baseUrl')}/usuarios/${defaultId}`,
+            body: {  email: 'empty.admin@qa.com.br', password: 'teste', administrador: '' },
+            expected: { status: 400, message: 'nome é obrigatório' },
+            property: 'nome'
         }
     ]
     
     errorCases.forEach(({ description, method, url, body, expected, property }) => {
-        it(`Bad Request - Should return error when ${description}`, () => {
+        it(`Bad Request - ${description}`, () => {
             cy.request({
                 method,
                 url,
